d4641fc2d54b28255daf3a37a15d3333a98eb6a0
CauaEduardo33 <caua.eduardo@edu.etefmc.com.br>
2026-06-10 21:13:11 -0300

    Enhance image classification with CORS support
    Added CORS middleware and improved image handling in the classification endpoint.

--- IA_disease_det/classificar.py
+++ IA_disease_det/classificar.py
@@ -1,35 +1,51 @@
-from fastapi.routing import request_response
-from fastapi import FastAPI
-import requests
-import json
+# É necessário enviar um File object, idealmente em FormData, porque é fácil de manipular
+
+
+
+from fastapi import FastAPI, UploadFile, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from ultralytics import YOLO
-from fastapi import UploadFile
+import io
+from PIL import Image
 
-
-
+model = YOLO("best.onnx")
 app = FastAPI()
 
-app.post("/classificar/")
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# caminho(url) completo é http://127.0.0.1:8000/classificar/, até hospedarmos em servidor, se o fizermos
+@app.post("/classificar/")
 async def classificar(imagem: UploadFile):
-    imagem_bytes = imagem.read()
-    model = YOLO("best.onnx")
-    results = model(imagem_bytes)
+    try:
+        contents = await imagem.read() # extrai as informações do arquivo
+        image = Image.open(io.BytesIO(contents)) # extrai os bytes da imagem para que o modelo consiga classificá-la
+        results = model(image)
 
-    for result in results:
-        names = [result.names[cls.item()] for cls in result.boxes.cls.int()] 
+        names = []
+        for result in results:
+            if result.boxes is not None:
+                names.extend([result.names[cls.item()] for cls in result.boxes.cls.int()])
 
-
-    return {
-        "resultado": names   
-    } 
-
-    
-
- 
-      
-
-
-
-    
-
-
+        return {
+          # Será enviado algo como : ["Rust", "Phoma", "Phoma"]. 
+          #Lógica para como o frontend va interpretar isso deve ser aplicado na camada intermediária
+            "resultado": names 
+        }
+        
+    except Exception as e:
+        print(f"Erro interno: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
